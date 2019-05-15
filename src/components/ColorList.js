83406be13c0851338631d8e2d75839d711756e33
Anna Ridenour <annamridenour@gmail.com>
2019-05-15 13:54:02 -0700

    in progress

--- src/components/ColorList.js
+++ src/components/ColorList.js
@@ -1,4 +1,19 @@
 import React from 'react';
 import Color from './Color';
+import PropTypes from 'prop-types';
 
+export default function ColorList({ colors }) {
+  ColorList.PropTypes = {
+    colors: PropTypes.array.isRequired
+  };
 
+  const colorList = colors.map(({ name, hex, rgb }) => {
+    
+  })
+
+  return (
+    <ul>
+      
+    </ul>
+  )
+}
