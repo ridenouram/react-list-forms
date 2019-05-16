9ff6106b9e56e32fedd750f955b3ef8da6b24569
Anna Ridenour <annamridenour@gmail.com>
2019-05-16 10:46:38 -0700

    sidebar is working and styled

--- src/components/App.js
+++ src/components/App.js
@@ -1,12 +1,29 @@
-import React from 'react';
+import React, { Component } from 'react';
 import Sidebar from './Sidebar';
+import styles from './Sidebar.css';
 
-export default function App() {
-  return (
-    <>
-    <Sidebar>
-      <a></a>
-    </Sidebar>
-    </>
-  );
+export default class App extends Component {
+  state = {
+    selected: 'dog'
+  }
+
+  handleChange = ({ target }) => {
+    this.setState({ selected: target.name });
+  }
+
+  render() {
+    const { selected } = this.state;
+    return (
+      <container className={styles.Container}>
+        <Sidebar>
+          <a href="#" name="dog" onClick={this.handleChange}>Dog</a>
+          <a href="#" name="cat" onClick={this.handleChange}>Cat</a>
+          <a href="#" name="snake" onClick={this.handleChange}>Snake</a>
+          <a href="#" name="fish" onClick={this.handleChange}>Fish</a>
+        </Sidebar>
+        <p>{selected}</p>
+      </container>
+    );
+
+  }
 }
--- src/components/Sidebar.js
+++ src/components/Sidebar.js
@@ -1,6 +1,15 @@
 import React from 'react';
 import PropTypes from 'prop-types';
+import styles from './Sidebar.css';
 
-export default function Sidebar({ childern }) {
-  
+export default function Sidebar({ children }) {
+  return (
+    <nav className={styles.Sidebar}>
+      {children}
+    </nav>
+  );
 }
+
+Sidebar.propTypes = {
+  children: PropTypes.node
+};
