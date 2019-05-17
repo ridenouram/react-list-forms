02a7e57c48ca61ce08aed90bfd543e24f78ac7e2
Anna Ridenour <annamridenour@gmail.com>
2019-05-17 09:58:02 -0700

    established components, wrote app.js

---
+++ README.md
@@ -0,0 +1,16 @@
+-create a Photo component
+-Photo takes a photo property and displays an img
+-create a Photos component
+-Photos takes an array of photos and creates a Photo for each
+-display all Photo components in a ul
+-create a PhotoAlbum component
+-PhotoAlbum takes a title and an array of photos as properties
+-display the title of the album
+-display all photos using the Photos component
+-create a Header component
+-display the title of your photo album site
+-create a Footer component
+-display your name in the footer
+-create an App component that brings these components together
+-create an index.js that renders your App component
+-NOTE use semantic HTML (header, footer, section, etc tags)
--- src/components/App.js
+++ src/components/App.js
@@ -1,1 +1,26 @@
+import React from 'react';
+import PhotoAlbum from './PhotoAlbum';
+import Header from './Header';
+import Footer from './Footer';
 
+export default function App() {
+  const photos = [
+    'https://www.dailydot.com/wp-content/uploads/2018/06/Possum.jpg',
+    'https://i.redd.it/i14dg5ylj9m11.jpg',
+    'https://static.boredpanda.com/blog/wp-content/uploads/2016/01/cute-possums-331__700.jpg',
+    'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTloMswUtwlLwegV3KidNCe7PYXQH-QYHC73pU15JkBl0IdYdr0',
+    'https://66.media.tumblr.com/221b1b8d7899e0d4d8ffc610e118ba3b/tumblr_ospa7oyIP21w4xt9wo1_1280.jpg',
+    'https://66.media.tumblr.com/cbdc8c596416dfa8453dec812174d196/tumblr_pb653uSoKz1w4xt9wo1_1280.jpg',
+    'https://66.media.tumblr.com/ac4e2e2107a4a61f8fff3a4c328374b4/tumblr_p9zou5mSY81w4xt9wo1_1280.jpg',
+    'https://66.media.tumblr.com/d61de80cb96e49dca84be214435777ac/tumblr_p9zos1zeSm1w4xt9wo1_500.jpg',
+    'https://i.pinimg.com/originals/ab/ac/85/abac8573ca3c2465b553a2cc0ac61a51.jpg'
+  ];
+  
+  return (
+    <>
+      <Header title='My Favorite Possums' />
+      <PhotoAlbum title='So Many Good Bois' photos={photos} />
+      <Footer title='Thanks for visiting my smokin hot photo album' />
+    </>
+  );
+}
